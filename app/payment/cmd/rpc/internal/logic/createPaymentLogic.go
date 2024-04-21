18a9944d5611aaa44f95eaa31fcab65b922534ef
goeer <3480340468@qq.com>
2024-04-21 16:46:56 +0800

    目前完成了支付的api和rpc

---
+++ app/payment/cmd/rpc/internal/logic/createPaymentLogic.go
@@ -0,0 +1,45 @@
+package logic
+
+import (
+	"context"
+
+	"looklook/app/payment/cmd/rpc/internal/svc"
+	"looklook/app/payment/cmd/rpc/pb"
+
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+type CreatePaymentLogic struct {
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+	logx.Logger
+}
+
+func NewCreatePaymentLogic(ctx context.Context, svcCtx *svc.ServiceContext) *CreatePaymentLogic {
+	return &CreatePaymentLogic{
+		ctx:    ctx,
+		svcCtx: svcCtx,
+		Logger: logx.WithContext(ctx),
+	}
+}
+
+// 创建微信支付预处理订单
+func (l *CreatePaymentLogic) CreatePayment(in *pb.CreatePaymentReq) (*pb.CreatePaymentResp, error) {
+	// todo: add your logic here and delete this line
+	data := new(model.ThirdPayment)
+	data.Sn = uniqueid.GenSn(uniqueid.SN_PREFIX_THIRD_PAYMENT)
+	data.UserId = in.UserId
+	data.PayMode = in.PayModel
+	data.PayTotal = in.PayTotal
+	data.OrderSn = in.OrderSn
+	data.ServiceType = model.ThirdPaymentServiceTypeHomestayOrder
+
+	_, err := l.svcCtx.ThirdPaymentModel.Insert(l.ctx,nil, data)
+	if err != nil {
+		return nil, errors.Wrapf(xerr.NewErrCode(xerr.DB_ERROR), "create wechat pay prepayorder db insert fail , err:%v ,data : %+v  ", err, data)
+	}
+
+	return &pb.CreatePaymentResp{
+		Sn: data.Sn,
+	}, nil
+}
